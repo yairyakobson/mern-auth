d16b56ff5980a3d01be7b0cc41b7f9edfcbd9533
yairyakobson <yairnet92@gmail.com>
2025-05-11 16:01:15 +0300

    delete: deleted un-necessary comment

--- backend/utils/cookies/main.ts
+++ backend/utils/cookies/main.ts
@@ -1,7 +1,6 @@
 import { CookieOptions } from "express";
 
 export const refreshPath = "/api/v1/refresh";
-// const secure = process.env.NODE_ENV !== "development";
 
 const defaults: CookieOptions = {
   sameSite: "strict",
