8d8c656c04a30106a7ada35504f02d2a4b07124e
yairyakobson <yairnet92@gmail.com>
2025-05-14 09:47:04 +0300

    modified: switched mailing system to resend

--- backend/controllers/mongoose/authentication/registration.ts
+++ backend/controllers/mongoose/authentication/registration.ts
@@ -9,7 +9,7 @@ import { refreshTokenSignOptions } from "../../../utils/jwt/refreshTokenSignOpti
 import { signToken } from "../../../utils/jwt/signToken";
 
 import VerificationCodeType from "../../../constants/verificationCodeType";
-import sendEmail from "../../../utils/email/sendEmail";
+import { sendEmail } from "../../../utils/email/sendEmail";
 
 export const createAccount = async(data: Authentication) =>{
   const user = await createUser({
@@ -24,20 +24,15 @@ export const createAccount = async(data: Authentication) =>{
     expiresAt: oneYearFromNow()
   });
 
-  try{
-    const url = `${APP_ORIGIN}/email/verify/${emailVerify._id}`;
-    const emailTemplate = getVerifyEmailTemplate(url);
+  const url = `${APP_ORIGIN}/email/verify/${emailVerify._id}`;
 
-    await sendEmail({
-      to: user.email,
-      subject: emailTemplate.subject,
-      text: emailTemplate.text,
-      html: emailTemplate.html
-    });
-  }
-  catch(error){
-    console.error(error);
-  }
+  // send verification email
+  const { error } = await sendEmail({
+    to: user.email,
+    ...getVerifyEmailTemplate(url),
+  });
+  // ignore email errors for now
+  if (error) console.error(error);
 
   const session = await createRegisterSession({
     userId,
@@ -62,3 +57,68 @@ export const createAccount = async(data: Authentication) =>{
     refreshToken
   }
 }
+
+// import { APP_ORIGIN } from "../../../constants/env";
+// import { createRegisterSession } from "../../../dataAccess/sessions";
+// import { createUser } from "../../../dataAccess/users";
+// import { createVerificationCode } from "../../../dataAccess/verification";
+// import { Authentication } from "../../../types/authenticationTypes";
+// import { oneYearFromNow } from "../../../utils/date";
+// import { getVerifyEmailTemplate } from "../../../utils/email/templates/verifyEmailTemplate";
+// import { refreshTokenSignOptions } from "../../../utils/jwt/refreshTokenSignOptions";
+// import { signToken } from "../../../utils/jwt/signToken";
+
+// import VerificationCodeType from "../../../constants/verificationCodeType";
+// import sendEmail from "../../../utils/email/sendEmail";
+
+// export const createAccount = async(data: Authentication) =>{
+//   const user = await createUser({
+//     email: data?.email,
+//     password: data?.password
+//   });
+//   const userId = user._id;
+
+//   const emailVerify = await createVerificationCode({
+//     userId,
+//     type: VerificationCodeType.EmailVerification,
+//     expiresAt: oneYearFromNow()
+//   });
+
+//   try{
+//     const url = `${APP_ORIGIN}/email/verify/${emailVerify._id}`;
+//     const emailTemplate = getVerifyEmailTemplate(url);
+
+//     await sendEmail({
+//       to: user.email,
+//       subject: emailTemplate.subject,
+//       text: emailTemplate.text,
+//       html: emailTemplate.html
+//     });
+//   }
+//   catch(error){
+//     console.error(error);
+//   }
+
+//   const session = await createRegisterSession({
+//     userId,
+//     userAgent: data?.userAgent
+//   });
+
+//   const accessToken = signToken({
+//     userId,
+//     sessionId: session?._id
+//   });
+
+//   const refreshToken = signToken(
+//     {
+//       sessionId: session?._id
+//     },
+//     refreshTokenSignOptions
+//   )
+
+//   return{
+//     user: user.omitPassword(),
+//     accessToken,
+//     refreshToken
+//   }
+// }
--- backend/utils/email/sendEmail.ts
+++ backend/utils/email/sendEmail.ts
@@ -6,31 +6,48 @@ import {
   SMTP_PASSWORD,
   SMTP_FROM_NAME,
   SMTP_FROM_EMAIL,
-  NODE_ENV
+  NODE_ENV,
+  EMAIL_SENDER
 } from "../../constants/env";
 import { EmailOptions } from "../../interfaces/emailInterface";
+import resend from "../../config/resend";
 
-const sendEmail = async(options: EmailOptions): Promise<void> =>{
-  const transport: Transporter = nodemailer.createTransport({
-    host: SMTP_HOST,
-    port: parseInt(SMTP_PORT, 10),
-    auth: {
-      user: SMTP_EMAIL,
-      pass: SMTP_PASSWORD
-    },
+const getFromEmail = () =>
+  NODE_ENV === "development" ? "onboarding@resend.dev" : EMAIL_SENDER;
+
+const getToEmail = (to: string) =>
+  NODE_ENV === "development" ? "delivered@resend.dev" : to;
+
+export const sendEmail = async ({ to, subject, text, html }: EmailOptions) =>
+  await resend.emails.send({
+    from: getFromEmail(),
+    to: getToEmail(to),
+    subject,
+    text,
+    html,
   });
 
-  const message = {
-    from: NODE_ENV === "development"
-    ? `${SMTP_FROM_NAME} <testmail@mail.com>`
-    : `${SMTP_FROM_NAME} <${SMTP_FROM_EMAIL}>`,
-    to: options.to,
-    subject: options.subject,
-    text: options.text,
-    html: options.html
-  };
-  
-  await transport.sendMail(message);
-};
+// const sendEmail = async(options: EmailOptions): Promise<void> =>{
+//   const transport: Transporter = nodemailer.createTransport({
+//     host: SMTP_HOST,
+//     port: parseInt(SMTP_PORT, 10),
+//     auth: {
+//       user: SMTP_EMAIL,
+//       pass: SMTP_PASSWORD
+//     },
+//   });
 
-export default sendEmail;
+//   const message = {
+//     from: NODE_ENV === "development"
+//     ? `${SMTP_FROM_NAME} <testmail@mail.com>`
+//     : `${SMTP_FROM_NAME} <${SMTP_FROM_EMAIL}>`,
+//     to: options.to,
+//     subject: options.subject,
+//     text: options.text,
+//     html: options.html
+//   };
+  
+//   await transport.sendMail(message);
+// };
+
+// export default sendEmail;
